fc687487d1368f29fc6a8f24a56384cc658ec8f2
Poliesti <italopoliesti@gmail.com>
2020-10-13 17:31:39 -0300

    att

--- R/read_file.R
+++ R/read_file.R
@@ -2,11 +2,8 @@ library(openxlsx)
 library(dplyr)
 library(tidyverse)
 
-path <- "C:/Users/italo/OneDrive/Área de Trabalho/"
+path <- "./arquivos/"
 
-nome_do_arquivo <- "a33476f9c9114c539dd22609812b59d8"
+nome_do_arquivo <- "a33476f9c9114c539dd22609812b59d8.xlsx"
 
-
-OPPA <- dplyr::as_tibble(read.xlsx(paste0(path, nome_do_arquivo, ".xlsx")))
-
-OPPA
+OPPA <- dplyr::as_tibble(read.xlsx(paste0(path, nome_do_arquivo)))
