67d264e2e65cf7eb3140738e16007c2fd1dc82a6
Arthur Lima de Araujo Miranda <60785806+ArthurLimaMiranda@users.noreply.github.com>
2020-10-13 17:00:54 -0300

    agr*VAI*

--- R/LerArqXlsx.R
+++ R/LerArqXlsx.R
@@ -1,6 +1,6 @@
-path_Entrada <- ""
-nome_do_arquivo_Entrada <- ""
+path_Entrada <- "./"
+nome_do_arquivo_Entrada <- "3bb640431c9942aa95754785dc5713d9.xlsx"
 
 OPPA <- dplyr::as_tibble(readxl::read_excel(paste0(path_Entrada,nome_do_arquivo_Entrada)))
 
-#OPPA
+OPPA
