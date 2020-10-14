1ecfb78aaba8397940e3ae078ec2932391b89410
Arthur Lima de Araujo Miranda <60785806+ArthurLimaMiranda@users.noreply.github.com>
2020-10-14 18:27:13 -0300

    Atualizado e pronto pro push

--- R/LerArqXlsx.R
+++ R/LerArqXlsx.R
@@ -1,5 +1,5 @@
-path_Entrada <- "./"
-nome_do_arquivo_Entrada <- "3bb640431c9942aa95754785dc5713d9.xlsx"
+path_Entrada <- "./inst/extdata/"
+nome_do_arquivo_Entrada <- "arquivo.xlsx"
 
 OPPA <- dplyr::as_tibble(readxl::read_excel(paste0(path_Entrada,nome_do_arquivo_Entrada)))
 
