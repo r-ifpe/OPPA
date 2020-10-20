2f68f7dc094ffab4219086a4ff30445266a96f33
Arthur Lima de Araujo Miranda <60785806+ArthurLimaMiranda@users.noreply.github.com>
2020-10-20 15:02:35 -0300

    Erros corrigidos

--- R/LerArqXlsx.R
+++ R/LerArqXlsx.R
@@ -1,6 +1,20 @@
-path_Entrada <- "./inst/extdata/"
-nome_do_arquivo_Entrada <- "arquivo.xlsx"
+#' Read XLSX files related to the OPPA project
+#'
+#' @param path The file's path to the XSLX file.
+#'
+#' @return A data frame.
+#'
+#' @details  By now, this function suports any XLSX file.
+#'
+#' @examples
+#' # these datasets are real ones. But it's here just for test purpose.
+#'
+#' # example selecting the file
+#' OPPA("E:/Back-up/Projetos - FNO/R projects/Ic/OPPA/inst/extdata/arquivo.xlsx")
+#'
+#' class(OPPA)
+#' @export
 
-OPPA <- dplyr::as_tibble(readxl::read_excel(paste0(path_Entrada,nome_do_arquivo_Entrada)))
-
-OPPA
+OPPA <- function(path){
+  dplyr::as_tibble(readxl::read_excel(path))
+}
